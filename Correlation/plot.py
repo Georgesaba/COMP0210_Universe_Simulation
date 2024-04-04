ad19869d350cd1837aedcac12a4f94b66c67a21e
Georgesaba <georgesaba@hotmail.co.uk>
2024-04-04 16:18:12 +0100

    Fixing Plots for Distributed Memory Correlations
    Fixed the axis and plot title's for all plot and changed the plot.py file to assign proper values to the correlation plot bins between 0 and 5.

--- Correlation/plot.py
+++ Correlation/plot.py
@@ -2,14 +2,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "Comparison_4_1.03_1.04"
+filename = "Comparison_4_1._1.04"
 
 data = pd.read_csv("Correlation/" + filename + ".csv").iloc[1:, :] #due to inf value
+step = 0.5/len(data)
+x = np.arange(0, 0.5, step)
+x += (x[1] - x[0])/2
 
 plt.figure()
-plt.title("Correlation/Correlation Plots for Different Expansion Factors")
+plt.title("Correlations for Different Expansion Factors")
+plt.xlabel("Radial Bin")
+plt.ylabel("Radial Correlation Function")
 for i in data.columns:
-   plt.plot(data[i],label = f"Expansion Factor: {i}")
+   plt.plot(x,data[i],label = f"Expansion Factor: {i}")
 plt.legend(loc='best')
 plt.grid(True)
 plt.savefig("Correlation/" + filename + ".png")
